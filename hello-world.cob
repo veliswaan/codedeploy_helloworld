41ec42f156993c1596cb8c6f7a8718486057be19
Veliswa Boya <33862731+veliswaan@users.noreply.github.com>
2021-07-19 15:58:06 +0200

    Add files via upload

---
+++ hello-world.cob
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CodeDeploytest.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 HELLO-X            PIC X(40).
+       PROCEDURE DIVISION.
+       DISPLAY "Hello Malta UG".
+       STOP RUN. 
+       
